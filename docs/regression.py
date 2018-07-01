d6ac6af4be7835e70c97b80cdffe8a51dd5f2748
Will Miller <williamjoemiller@gmail.com>
2018-07-01 17:56:32 -0400

    added matplotlib graph and plotted forecast values

--- docs/regression.py
+++ docs/regression.py
@@ -4,10 +4,14 @@
 # hopefully a strong prediction of a future value. Usually want between -1 and 1.
 
 import pandas as pd
-import quandl, math
+import quandl, math, datetime
 import numpy as np
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -35,23 +39,45 @@ forecast_out = int(math.ceil(0.01*len(df)))
 # shifting columsn in a negative manner so that each label column of each row
 # will be the adjusted row price 10 days into the future
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
-
 
 # Now going to define features and pass them to a classifier
 # X = features; y = labels
 
-X = np.array(df.drop(['label'], 1)) #returns new dataframe
-y = np.array(df['label'])
+X = np.array(df.drop(['label'], 1)) # returns new dataframe
+X = preprocessing.scale(X) # scale from -1 to 1, would be skipped in high frequency trading
+X = X[:-forecast_out]
+X_lately = X[-forecast_out:] # stuff we are going to predict against
 
-X = preprocessing.scale(X) #scale from -1 to 1, would be skipped in high frequency trading
 df.dropna(inplace=True)
+y = np.array(df['label'])
 y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
 
 # Define classifier
 clf = LinearRegression(n_jobs=-1) #97% accuracy
-
 clf.fit(X_train, y_train)
 accuracy = clf.score(X_test, y_test)
+
+forecast_set = clf.predict(X_lately)
+print(forecast_set, accuracy, forecast_out)
+df['Forecast'] = np.nan
+
+# Handling time, date is not a feature in this data
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+# Set forecast values
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4) #bottom right
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show() # show plotted graph
