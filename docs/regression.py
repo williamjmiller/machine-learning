5c6b07f7c7ee9612e1311d048501b808d36e350a
Will Miller <williamjoemiller@gmail.com>
2018-07-01 17:31:08 -0400

    specified features and fit linear regression model ~ 97% accuracy

--- docs/regression.py
+++ docs/regression.py
@@ -1,11 +1,13 @@
 # Regression - Algorithm for Stock Prices
 
 # Note: Features are the attributes that make up the label, and the label is
-# hopefully a strong prediction of a future value
+# hopefully a strong prediction of a future value. Usually want between -1 and 1.
 
 import pandas as pd
-import quandl
-import math
+import quandl, math
+import numpy as np
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -34,4 +36,22 @@ forecast_out = int(math.ceil(0.01*len(df)))
 # will be the adjusted row price 10 days into the future
 df['label'] = df[forecast_col].shift(-forecast_out)
 df.dropna(inplace=True)
-print(df.head())
+
+
+# Now going to define features and pass them to a classifier
+# X = features; y = labels
+
+X = np.array(df.drop(['label'], 1)) #returns new dataframe
+y = np.array(df['label'])
+
+X = preprocessing.scale(X) #scale from -1 to 1, would be skipped in high frequency trading
+df.dropna(inplace=True)
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+
+# Define classifier
+clf = LinearRegression(n_jobs=-1) #97% accuracy
+
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
